9116785761db415052550b0de29aa89506fcbd67
NishthaSaraswat2001 <nishthasaraswat15@gmail.com>
2021-08-19 13:57:56 +0530

    default filters screen for testing

---
+++ lib/screeen/filters_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({Key? key}) : super(key: key);
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filters'),
+      ),
+      body: Center(
+        child: Text('filters come here'),
+      ),
+    );
+  }
+}
