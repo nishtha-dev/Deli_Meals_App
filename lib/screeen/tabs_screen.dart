0e31172cd5b3ffa55544869b27fd18dc9ee38391
NishthaSaraswat2001 <nishthasaraswat15@gmail.com>
2021-08-19 13:26:10 +0530

    bottom tabbar implementation

--- lib/screeen/tabs_screen.dart
+++ lib/screeen/tabs_screen.dart
@@ -2,6 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/screeen/categories_screen.dart';
 import 'package:meals_app/screeen/favourite_screen.dart';
 
+// class TabsScreen extends StatefulWidget {
+//   const TabsScreen({Key? key}) : super(key: key);
+
+//   @override
+//   _TabsScreenState createState() => _TabsScreenState();
+// }
+
+// class _TabsScreenState extends State<TabsScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//         length: 2,
+//         child: Scaffold(
+//           appBar: AppBar(
+//             title: Text('Deli Meals'),
+//             bottom: TabBar(
+//               tabs: [
+//                 Tab(
+//                   icon: Icon(Icons.category),
+//                   text: 'Categories',
+//                 ),
+//                 Tab(
+//                   icon: Icon(Icons.star),
+//                   text: 'favourites',
+//                 )
+//               ],
+//             ),
+//           ),
+//           body: TabBarView(children: <Widget>[
+//             CategoriesScreen(),
+//             FavoritesScreen(),
+//           ]),
+//         ));
+//   }
+// }
+
 class TabsScreen extends StatefulWidget {
   const TabsScreen({Key? key}) : super(key: key);
 
@@ -10,30 +46,46 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Widget> _pages = [
+    CategoriesScreen(),
+    FavoritesScreen(),
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectedPage(index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Deli Meals'),
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: 'Categories',
-                ),
-                Tab(
-                  icon: Icon(Icons.star),
-                  text: 'favourites',
-                )
-              ],
-            ),
-          ),
-          body: TabBarView(children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ]),
-        ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('meals screen'),
+      ),
+      body: _pages[_selectedPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectedPage,
+        currentIndex: _selectedPageIndex,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        // unselectedFontSize: 10,
+        // selectedFontSize: 10,
+        //type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+              // backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category),
+              title: Text('Categories')),
+          BottomNavigationBarItem(
+              // backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.star),
+              title: Text('Favourites')),
+        ],
+      ),
+    );
   }
 }
