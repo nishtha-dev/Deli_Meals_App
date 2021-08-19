98a76d0d747a7085a394036a568b4c73fba0841c
NishthaSaraswat2001 <nishthasaraswat15@gmail.com>
2021-08-19 12:58:12 +0530

    removed scaffold

--- lib/screeen/categories_screen.dart
+++ lib/screeen/categories_screen.dart
@@ -7,22 +7,17 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Deli Meals'),
-      ),
-      body: GridView(
-          padding: const EdgeInsets.all(15),
-          children: DUMMY_CATEGORIES
-              .map((catData) =>
-                  CategoryItem(catData.title, catData.color, catData.id))
-              .toList(),
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200, //fix max size of one grid element
-            childAspectRatio: 3 / 2, //height :width ratio of one element
-            crossAxisSpacing: 20, //space b/t 2 columns
-            mainAxisSpacing: 20, //space b/t 2 rows
-          )),
-    );
+    return GridView(
+        padding: const EdgeInsets.all(15),
+        children: DUMMY_CATEGORIES
+            .map((catData) =>
+                CategoryItem(catData.title, catData.color, catData.id))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200, //fix max size of one grid element
+          childAspectRatio: 3 / 2, //height :width ratio of one element
+          crossAxisSpacing: 20, //space b/t 2 columns
+          mainAxisSpacing: 20, //space b/t 2 rows
+        ));
   }
 }
